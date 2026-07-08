863045937bc1f7eda6289da617dc9922d80c7285
Atharva <atharvakumbhar631@gmail.com>
2026-07-08 22:53:02 +0530

    input for user

---
+++ DAY2/input1.py
@@ -0,0 +1,10 @@
+#Take input from user and display.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Your first name is :", first_name)
+print("Your last name is :", last_name)
+
+full_name= first_name +" "+last_name
+print("Your full name is =", full_name)
