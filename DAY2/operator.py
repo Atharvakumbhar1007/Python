bc4a579c77c40336303d8a579fe94434b1e14172
Atharva <atharvakumbhar631@gmail.com>
2026-07-08 23:15:23 +0530

    Operations

---
+++ DAY2/operator.py
@@ -0,0 +1,19 @@
+#Operators in Python
+print("Arithmetic Operators")
+
+'''
+*-> Multiplication
+/-> Division
+//-> Floor Division/Integer Division
+%-> Modulus
++-> Addition
+- -> Subtraction
+**-> Exponentiation/Raise to power
+'''
+n1 = 10
+n2 = 5
+print("Multiplication:", n1*n2) #Multiplication
+print("Division:", n1/n2) #Division
+print("Floor Division:", n1//n2) #Floor Division
+print("Modulus:", n1%n2) #Modulus
+print("Exponentiation:", n1**n2) #Exponentiation
