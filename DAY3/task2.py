c815a7628f742349da7e20fd8bb7ed54d1e18e92
Atharva <atharvakumbhar631@gmail.com>
2026-07-10 22:17:53 +0530

    task 2

---
+++ DAY3/task2.py
@@ -0,0 +1,17 @@
+#WAP to Accept/input marks of 3 subject print sum and average
+# m1 = 55, m2 = 80, m3 = 95
+#sum = m1+m2+m3 = 230
+#avg = sum / 3 = 80.22
+#Output = sum of mrks is 193 and average is 80.22
+
+marks1 = int(input("Enetr Marks of Subject 1: "))
+
+marks2 = int(input("Enetr Marks of Subject 2: "))
+
+marks3 = int(input("Enetr Marks of Subject 3: "))
+
+sum = marks1 + marks2 + marks3
+
+avg = sum / 3
+
+print(f"Sum of marks is {sum} and average is {avg}")
