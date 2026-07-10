2031ec7b4ca85201f255d9f41bfeae0048a33915
Atharva <atharvakumbhar631@gmail.com>
2026-07-10 22:09:45 +0530

    task

--- DAY2/operator.py
+++ DAY2/operator.py
@@ -17,3 +17,12 @@ print("Division:", n1/n2) #Division
 print("Floor Division:", n1//n2) #Floor Division
 print("Modulus:", n1%n2) #Modulus
 print("Exponentiation:", n1**n2) #Exponentiation
+
+n1=11
+n2=4
+print("Multiplication:",n1,"*",n2,"=",n1*n2) #Multiplication
+#Formatting Strings -> fstrings
+print("Multiplication ->{n1} * {n2} ={n1*n2}")
+print("Division ->{n1} / {n2} ={n1/n2}")
+print("Floor division ->{n1} // {n2} ={n1//n2}")
+print("Modulus ->{n1} % {n2} ={n1%n2}")
---
+++ DAY3/task1.py
@@ -0,0 +1,24 @@
+#Example
+#Input: 68
+#Output: Temperature 68 Fare
+
+# Celsius = (Fahrenheit - 32) * 5 / 9
+
+temp_in_farenheit = float(input("Enter temperatur in farenheit: "))
+
+#formaula
+temp_in_farenheit = temp_in_farenheit - 32 * 5/ 9
+
+#Display Answer
+print(f"Temperatur {temp_in_farenheit} Farenheitis {temp_in_farenheit} Degree Celcius")
+
+
+
+
+
+
+
+
+
+
+
