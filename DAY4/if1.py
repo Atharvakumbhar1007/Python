16bf861880bf6838c0e0d3160b590f9dd32e8132
Atharva <atharvakumbhar631@gmail.com>
2026-07-12 16:28:47 +0530

    if else

--- DAY4/if1.py
+++ DAY4/if1.py
@@ -1,6 +1,8 @@
 # WAP to check whether the number is positive
-
+#Simple if
 num = int(input(f"Enter a number: "))
 
 if num > 0:
     print("Number is positive")
+    
+    
---
+++ DAY4/ifelse1.py
@@ -0,0 +1,10 @@
+#if-else condition
+
+# WAP to check positive or negative
+
+num = int(input("Enter a number: "))
+
+if num >= 0:
+    print("No. is positive")
+else:
+    print("No. is negative")
