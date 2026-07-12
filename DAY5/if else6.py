5ae1588f7c4c548fadbd4c8f795bd2709a5c5492
Atharva <atharvakumbhar631@gmail.com>
2026-07-12 17:30:20 +0530

    if else 6

---
+++ DAY5/if else6.py
@@ -0,0 +1,10 @@
+# WAP to check whether the number is positive, negative, or zero
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Number is Positive")
+elif num < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
