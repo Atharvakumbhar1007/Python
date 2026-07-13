a0054a6e98631665e6a210bbad7211a1675b9e44
Atharva <atharvakumbhar631@gmail.com>
2026-07-13 10:27:58 +0530

    ifelse13

--- DAY6/ifelse12.py
+++ DAY6/ifelse12.py
@@ -9,13 +9,16 @@ year % 4 == 0
 2. Century years (100,200,..1800,2000)
 a.Not all century are leap year -> Not divisible by 100
 -> year % 100 != 0
+
+                    OR
+
 b.Century years that repeats after every 400 year are leap year
--> 
+-> Divisible by 400 -> year-> year % 400 == 0
 '''
 
 year = int(input("Enter a year: "))
 
-if year % 4 == 0 and (year % 100 != 0):
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print(f"Year = {year} is a leap year")
 else:
     print(f"Year = {year} is not a leap year")
